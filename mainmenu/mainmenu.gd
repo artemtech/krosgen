51d6d310bf34a4cd4d123d734780957f744360a1
artemtech <sofyanartem@gmail.com>
2017-09-10 09:31:32 +0700

    initial commit

---
+++ mainmenu/mainmenu.gd
@@ -0,0 +1,39 @@
+extends Control
+
+export(float) var screen_move_speed = 500
+
+var current_screen = "start"
+var old_size = Vector2()
+
+onready var pengaturan = get_node("settings")
+onready var tween = get_node("tween")
+
+func _ready():
+	for components in get_node("main/buttons").get_children():
+		if components extends BaseButton:
+			if components.get_name() == "exitButton" :
+				components.connect("pressed", self, "_exitAction")
+			else:
+				components.connect("pressed", self, "_targets", [components.get_name()])
+	
+	for node in get_children():
+		if node.has_node("tutupButton"):
+			node.get_node("tutupButton").connect("pressed", self, "_targets", ["start"])
+
+func _targets(var screen = "start"):
+	current_screen = screen
+	
+	var target_coordinates = Vector2(0, 0) # By default, use the 0, 0 coordinates
+	if has_node(screen):
+		target_coordinates = get_node(screen).get_pos() # If possible, use the coordinates of the target
+	
+	var current_coordinates = get_pos()
+	var distance = current_coordinates.distance_to(target_coordinates)
+	var time = distance/screen_move_speed
+	
+	if time > 0:
+		tween.interpolate_property(self, "rect/pos", current_coordinates, -target_coordinates, time, Tween.TRANS_EXPO, Tween.EASE_OUT, 0)
+		tween.start()
+
+func _exitAction():
+	get_tree().quit()
