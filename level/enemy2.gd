fa98ff8fe963efbb656459fb99f38abb569827e6
artemtech <sofyanartem@gmail.com>
2017-09-25 01:29:49 +0700

    update enemy movement

--- level/enemy2.gd
+++ level/enemy2.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
 var degPutar= 0;
+export var speedGerak = 100;
 
 func _ready():
 	set_fixed_process(true);
 
 func _fixed_process(delta):
-	degPutar+= 90 * delta;
+	move(Vector2(1,0)*speedGerak*delta);
+	degPutar= fmod(degPutar+90 * delta,360);
 	print(degPutar);
